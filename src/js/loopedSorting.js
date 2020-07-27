8187ba72fcffaf30e7e1dc5920b2252406cd6f45
Vakabunga <shesterov@gmail.com>
2020-07-28 00:51:33 +0300

    create looped sorting function; !TODO: create eventListener to switch between types of sorting

--- src/js/loopedSorting.js
+++ src/js/loopedSorting.js
@@ -1,7 +1,18 @@
+import tableSort from './tableSort.js';
 // функция запуска автоматической периодической сортировки
+
 export default function loopedSorting() {
-  let a = 1;
-  if (a > 1) {
-    a = 5;
+  const timer = () => {
+    const e = new Date().getTime() + 2000;
+    // eslint-disable-next-line no-empty
+    while (new Date().getTime() <= e) {}
+  };
+  const filter = ['id', 'title', 'imdb', 'year'];
+  const direction = ['up', 'down'];
+  for (let i = 0; i < filter.length; i += 1) {
+    for (let j = 0; j < direction.length; j += 1) {
+      tableSort(i, j);
+      timer();
+    }
   }
 }
